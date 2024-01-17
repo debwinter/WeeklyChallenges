1affe871578c885196128fe21151696a5fd46a00
Debra Winter <deb@debwinter.com>
2024-01-17 11:15:34 -0500

    attempted exercise 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,31 +18,44 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
+            return minuend - subtrahend;
             throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
         {
+            return number1 + number2;
             throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else if (number2 > number1)
+            {
+                return number1;
+            }
             throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
+            return factor1 * factor2;
             throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
+            return $"Hello, {nameOfPerson}!";
             throw new NotImplementedException();
         }
 
         public string GetHey()
         {
+            return "Hey";
             throw new NotImplementedException();
         }
     }
