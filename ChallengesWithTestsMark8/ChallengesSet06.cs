672809f9a4e4970a9cbbd5f25f38e8f54f4faddd
Debra Winter <deb@debwinter.com>
2024-02-28 13:09:08 -0500

    weekly challenges 6

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -7,27 +7,86 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null) return false;
+            bool iFoundIt = false;
+            foreach (var w in words)
+            {
+                if (w == word) iFoundIt = true;
+                else if (ignoreCase && w != null && w.ToLower() == word.ToLower()) iFoundIt = true;
+            }
+            return iFoundIt;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num == null || num <= 1) return false;
+            bool isPrime = true;
+            for (int i = num - 1; i > 1; i--) if (num % i == 0) isPrime = false;
+            return isPrime;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
-        }
-
-        public int MaxConsecutiveCount(int[] numbers)
-        {
-            throw new NotImplementedException();
+            if (str.Length == 1) return 0;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                bool foundMatch = false;
+                for (int x = 0; x < str.Length; x++)
+                {
+                    if (x == i) continue;
+                    else if (str[i] == str[x])
+                    {
+                        foundMatch = true;
+                        break;
+                    }
+                }
+                if (!foundMatch) return i;
+            }
+            return -1;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || n <= 0) return new double[] { };
+            var everyNth = new double[elements.Count / n];
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (i % n == n - 1)
+                {
+                    everyNth[i / n] = elements[i];
+                }
+            }
+            return everyNth;
+        }
+
+        public int MaxConsecutiveCount(int[] numbers)
+        {
+            int counter1 = 1;
+            int counter2 = 1;
+            int maxcounter = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1]) counter1++;
+                else
+                {
+                    for (int x = i+1; x < numbers.Length - 1; x++)
+                    {
+                        if (numbers[x] == numbers[x + 1]) counter2++;
+                        else
+                        {
+                            i = x;
+                            break;
+                        }
+                    }
+                    if (Math.Max(counter1, counter2) > maxcounter) maxcounter = Math.Max(counter1, counter2);
+                    counter1 = 1;
+                    counter2 = 1;
+                }
+            }
+            if (Math.Max(counter1, counter2) > maxcounter) maxcounter = Math.Max(counter1, counter2);
+            if (maxcounter == 1) return 0;
+            else return maxcounter;
         }
     }
 }
--- ChallengesWithTestsMark8/Program.cs
+++ ChallengesWithTestsMark8/Program.cs
@@ -6,7 +6,42 @@ namespace ChallengesWithTestsMark8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            //Console.WriteLine("Hello, world!");
+
+            var c6 = new ChallengesSet06();
+            //Console.WriteLine("Array: { 1, 2, 2, 3, 4, 5 }");
+            //Console.WriteLine("Expected: 2");
+            //Console.WriteLine($"Actual: {c6.MaxConsecutiveCount(new int[] { 1, 2, 2, 3, 4, 5 })}");
+            //Console.WriteLine();
+
+            //Console.WriteLine("Array: { 10, 3, 1, 1, 1, 5, 1 }");
+            //Console.WriteLine("Expected: 3");
+            //Console.WriteLine($"Actual: {c6.MaxConsecutiveCount(new int[] { 10, 3, 1, 1, 1, 5, 1 })}");
+            //Console.WriteLine();
+
+            //Console.WriteLine("Array: { 1, 2, 2, 3, 3, 3, 3, 2, 5 }");
+            //Console.WriteLine("Expected: 4");
+            //Console.WriteLine($"Actual: {c6.MaxConsecutiveCount(new int[] { 1, 2, 2, 3, 3, 3, 3, 2, 5 })}");
+            //Console.WriteLine();
+
+            Console.WriteLine("Array: { 1, 2, 2, 3, 3, 3, 3 }");
+            Console.WriteLine("Expected: 4");
+            Console.WriteLine($"Actual: {c6.MaxConsecutiveCount(new int[] { 1, 2, 2, 3, 3, 3, 3 })}");
+            Console.WriteLine();
+
+            //Console.WriteLine("Array: { 1, 2, 2, 3, 2 }");
+            //Console.WriteLine("Expected: 2");
+            //Console.WriteLine($"Actual: {c6.MaxConsecutiveCount(new int[] { 1, 2, 2, 3, 2 })}");
+            //Console.WriteLine();
+
+            //Console.WriteLine("Array: { 3, 3, 3, 2, 2 }");
+            //Console.WriteLine("Expected: 3");
+            //Console.WriteLine($"Actual: {c6.MaxConsecutiveCount(new int[] { 3, 3, 3, 2, 2 })}");
+            //Console.WriteLine();
+
+            //Console.WriteLine("Array: { 1, 3, 2, 3, 3, 3, 3 }");
+            //Console.WriteLine($"Actual: {c6.MaxConsecutiveCount(new int[] { 1, 3, 2, 3, 3, 3, 3 })}");
+            //Console.WriteLine("Expected: 4");
         }
     }
 }
